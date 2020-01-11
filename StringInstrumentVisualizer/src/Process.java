9508ec196142324f687cb2da2fa8eadfed7c4de7
jackGolden <jackGolden@jackGolden>
2020-01-11 15:40:25 -0500

    Second commit

---
+++ StringInstrumentVisualizer/src/Process.java
@@ -0,0 +1,9 @@
+import processing.core.PApplet;
+
+public class Process extends PApplet {
+	private static final long serialVersionUID = 1L;
+	
+	public static void main(String[] args) {
+		PApplet.main("Visualizer");
+	}
+}
