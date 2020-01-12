7b2b29dad9c38e6aef7f56e5aca29ecf3fb6a669
jackgolden <jackgolden@jacks-mbp-2>
2020-01-12 16:49:27 -0500

    Defined NoteSet

---
+++ StringInstrumentVisualizer/src/NoteSet.java
@@ -0,0 +1,9 @@
+public class NoteSet {
+	private int[] notes;
+	private String name;
+	public NoteSet(int[] notes, String name)
+	{
+		this.notes = notes;
+		this.name = name;
+	}
+}
--- StringInstrumentVisualizer/src/Process.java
+++ StringInstrumentVisualizer/src/Process.java
@@ -7,7 +7,7 @@ public class Process extends PApplet {
 		PApplet.main("Process");
 	}
 
-	int[] minorScale = { 0, 2, 3, 5, 7, 8, 10 };
+	NoteSet minorScale = new NoteSet({ 0, 2, 3, 5, 7, 8, 10 }, "Minor Scale");
 	int[] majorScale = { 0, 2, 4, 5, 7, 9, 11 };
 	int[] minorPent = { 0, 3, 5, 7, 10 };
 	int[] majorPent = { 0, 2, 5, 7, 9 }; // these are rootless shapes. shift to align with different roots on guitar.
@@ -19,6 +19,7 @@ public class Process extends PApplet {
 
 	int[] scale = majorScale; // ************CHOOSE YOUR SCALE**********************
 	String scaleName = "Major Scale";
+	int scaleStart = 0;
 
 	int[] ukeN = { 5, 0, 8, 3 };
 	int[] bassN = { 3, 10, 5, 0 };
@@ -33,10 +34,10 @@ public class Process extends PApplet {
 		size(1200, 600);
 		background(0);
 		nFrets = 17;
-		startpointx = width / 8;
+		startpointx = width / 12;
 		startpointy = height / 4;
 
-		lengthx = 6 * width / 8;
+		lengthx = 10 * width / 12;
 		lengthy = height / 2;
 	}
 
@@ -47,6 +48,21 @@ public class Process extends PApplet {
 
 	public void draw() {
 		renderNotes();
+	}
+	
+	public void keyPressed()
+	{
+	  if(key == 'i'){  //switch instrument
+	      
+	  }
+	  if(key == 's'){   //switch scale
+	  
+	  }
+	  else if(key == CODED)     //left right arrow keys move KEY of song
+	  {
+	    scaleStart = (scaleStart + 1) % 12;
+	  }
+	  
 	}
 
 	public void renderStrings() {
@@ -95,23 +111,21 @@ public class Process extends PApplet {
 	}
 
 	public void renderNotes() {
-		for (int string = 0; string < instrument.length; string++)
-			for (int fret = 0; fret < nFrets - 1; fret++)
-				for (int i = 0; i < scale.length; i++) {
-					if (scale[i] == (guitarN[string] + fret) % 12) {
-						int xcord = startpointx + fret * lengthx / (nFrets - 1) + lengthx / (nFrets * 2);
-						int ycord = startpointy + string * lengthy / (instrument.length - 1);
-
-						if (xcord < mouseX + 2 * lengthx / 17 && xcord > mouseX - 3 * lengthx / 17
-								&& mouseY < startpointy + lengthy && mouseY > startpointy) // mouse must be inside fret
-																							// board
-							fill(0, 255, 0);
-						if ((fret + guitarN[string]) % 12 == 0)
-							fill(255, 0, 0);
-						circle(xcord, ycord, 20);
-						fill(200);
-						break;
-					}
-				}
+	      for (int string = 0; string < instrument.length; string++)
+	          for (int fret = 0; fret < nFrets - 1; fret++)
+	            for (int i = 0; i < scale.length; i++) {
+	              if (scale[i] == (guitarN[string] + fret) % 12) {
+	                int xcord = startpointx + fret * lengthx / (nFrets - 1) + lengthx/(nFrets * 2);
+	                int ycord = startpointy + string * lengthy / (instrument.length - 1);
+	                
+	                if(xcord < mouseX + 2 * lengthx/17 && xcord > mouseX - 3*lengthx/17 && mouseY < startpointy + lengthy && mouseY > startpointy) //mouse must be inside fret board
+	                 fill(0,255, 0);
+	                if( (fret+ guitarN[string]) % 12 ==0)
+	                  fill(255,0,0);
+	                circle(xcord, ycord, 20);
+	                fill(200);
+	                break;
+	              }
+	            }
 	}
 }
